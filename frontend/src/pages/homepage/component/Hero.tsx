33f3948d87a17656f55a72b34c2e46a40385c0f9
HaziqAjieq <muhd.haziq.khair@gmail.com>
2026-02-18 12:40:22 +0800

    finish hero section

--- frontend/src/pages/homepage/component/Hero.tsx
+++ frontend/src/pages/homepage/component/Hero.tsx
@@ -2,22 +2,23 @@ import heroImg from "../../../assets/image/heroImage/test2.png";
 
 const Hero = () => {
   return (
-    <section className="flex items-center justify-center pt-30 lg:pt-20 mb-10 md:pt-40  h-screen">
-     <div className="flex flex-col gap-5 sm:gap-0 sm:grid  sm:grid-cols-2 lg:grid-cols-3 h-full">
+    <section className="flex  items-center justify-center pt-30 lg:pt-20 mb-10 md:pt-40  h-screen overflow-hidden">
+     <div className="flex flex-col justify-evenly gap-5 lg:grid lg:grid-cols-3 h-full">
 
      
-        <div className=" flex items-center w-full justify-center">
-          <h1 className="text-center w-full font-bold font-title text-3xl m-3">"Learning, Building, Growing"</h1>
+        <div className=" flex items-center w-full   ">
+          <h1 className="text-center  w-full font-bold font-title text-3xl md:text-6xl m-3 text-text-primary">"Learning, Building, Growing"</h1>
         </div>
-        <div className="lg:order-3 flex items-center">
-          <h2 className="text-center mx-5 sm:text-start font font-medium font-paragraph text-md ">
+        <div className=" flex flex-col  justify-center lg:order-3 ">
+          <h2 className="text-center mx-5 lg:text-start font font-medium font-paragraph text-sm md:text-xl text-text-secondary ">
           "I’m Haziq, exploring how to create websites step by step. Here you’ll find the projects I’m working on as I grow my skills."
           </h2>
+         
         </div>
 
         {/* image on the first layer */}
         <div className="h-full sm:col-span-2 lg:col-span-1 lg:order-2 flex  items-end  justify-center ">
-          <img src={heroImg} className="w-80 md:w-80 lg:w-120 " />
+          <img src={heroImg} className="w-120 md:180  lg:w-full " />
         </div>
    </div>
     </section>
