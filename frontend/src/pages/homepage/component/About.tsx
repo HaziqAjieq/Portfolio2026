8e41f519b5f55174d5947ca945adcf2d44519607
HaziqAjieq <muhd.haziq.khair@gmail.com>
2026-02-26 09:52:40 +0800

    finish about me section

--- frontend/src/pages/homepage/component/About.tsx
+++ frontend/src/pages/homepage/component/About.tsx
@@ -4,18 +4,18 @@ const About = () => {
   const { about } = homepageContent;
 
   return (
-    <section className="about bg-gray-50 py-24  overflow-hidden">
-      <div className="max-w-6xl mx-auto px-6 z-10">
+    <section className="about flex flex-col items-center bg-gray-50 py-24    ">
+      <div className="max-w-6xl mx-full px-8  ">
         
         {/* Title */}
-        <h2 className="font-title font-bold text-3xl md:text-4xl text-text-primary text-center mb-16">
+        <h2 className="flex  items-center justify-center md:hidden font-title font-bold text-4xl md:text-4xl text-text-primary text-center mb-10">
           {about.title}
         </h2>
 
         <div className="flex flex-col md:flex-row items-center gap-12">
           
           {/* Image */}
-          <div className="md:w-1/2 flex justify-center md:justify-start ">
+          <div className="w-full md:w-1/2 flex justify-center  ">
             <img
               src={about.imageUrl}
               alt="About Image"
@@ -24,25 +24,35 @@ const About = () => {
                 md:w-120 
                 lg:w-130
                 object-contain
-
+                border-none
+                rounded-full
                 md:-ml-10
+                bg-surface
+                shadow-bottom
+                h-full
               "
             />
           </div>
 
           {/* Text */}
-          <div className="md:w-1/2 flex flex-col gap-6 text-center md:text-left">
+          <div className="md:w-1/2 flex flex-col gap-2 text-center md:text-left">
+            <h2 className="hidden md:flex items-center justify-center font-title font-bold text-3xl md:text-4xl text-text-primary text-center mb-10">
+          {about.title}
+        </h2>
+
             <p className="font-paragraph text-text-secondary leading-8 max-w-xl mx-auto md:mx-0">
               {about.description}
             </p>
 
-            <button className="self-center md:self-start bg-primary text-white px-6 py-3 rounded-xl shadow-md hover:bg-secondary hover:-translate-y-1 transition-all duration-300">
+            <button className="self-center md:self-start bg-primary-accent text-white px-6 py-3 rounded-xl shadow-md hover:bg-secondary hover:-translate-y-1 transition-all duration-300">
               {about.ctaButton}
             </button>
           </div>
 
         </div>
+     
       </div>
+        <div className="relative bottom-0 left-20 md:left-40 transform -translate-x-1/2 w-3/5 h-1 bg-boder mt-10 md:mt-20"></div>
     </section>
   );
 };
--- frontend/src/pages/homepage/component/Hero.tsx
+++ frontend/src/pages/homepage/component/Hero.tsx
@@ -5,15 +5,15 @@ import { homepageContent } from "../../../content/homepage.type";
 const Hero = () => {
   const { hero } = homepageContent ;
   return (
-    <section className="hero flex flex-wrap  items-center justify-center pt-30  mb-10   h-screen overflow-hidden ">
+    <section className="hero flex   items-center justify-center pt-30  mb-10   h-screen overflow-hidden ">
      <div className="flex flex-col justify-evenly gap-5 lg:grid lg:grid-cols-[1fr_1.5fr_1fr] h-full">
 
      
         <div className=" flex items-center    ">
-          <h1 className="text-center  w-full font-bold font-title text-3xl md:text-6xl m-3 text-text-primary">{hero.title}</h1>
+          <h1 className="text-center  w-full font-bold font-title text-4xl md:text-6xl m-3 text-text-primary">{hero.title}</h1>
         </div>
-        <div className=" flex flex-col  justify-center lg:order-3 ">
-          <h2 className="text-center mx-5 lg:text-start font font-medium font-paragraph text-md md:text-xl text-text-secondary ">
+        <div className=" flex flex-col  justify-center items-center lg:order-3 w-full ">
+          <h2 className=" mx-2 text-center justify-center text-balance  lg:text-start font font-medium font-paragraph text-md md:text-xl text-text-secondary ">
          {hero.subtitle}
           </h2>
          
@@ -21,7 +21,7 @@ const Hero = () => {
 
         {/* image on the first layer */}
         <div className=" md:h-full  sm:col-span-2 lg:col-span-1 lg:order-2 flex  items-end  justify-center  ">
-          <img src={heroImg} className="  h-auto w-80 sm:w-90 md:w-160 lg:w-full rounded-b-full  " />
+          <img src={heroImg} className="  h-auto w-80 sm:w-90 md:w-120 lg:w-full rounded-b-full  " />
         </div>
    </div>
     </section>
